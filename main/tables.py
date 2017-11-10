74cba8ae3fdf6ab982590a7f740581808b6b6082
Gahan <gahan@quixom.com>
2017-11-10 15:23:18 +0530

    changed verbose name in view employee data table

--- main/tables.py
+++ main/tables.py
@@ -4,8 +4,8 @@ import django_tables2 as tables
 
 class EmployeeDataTable(tables.Table):
     first_name = tables.Column(accessor='user.first_name', verbose_name='First Name')
-    last_name = tables.Column(accessor='user.last_name', verbose_name='First Name')
-    email = tables.Column(accessor='user.email', verbose_name='First Name')
+    last_name = tables.Column(accessor='user.last_name', verbose_name='Last Name')
+    email = tables.Column(accessor='user.email', verbose_name='Email Address')
 
 
     class Meta:
