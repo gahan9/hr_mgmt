80a3471ff08478e6e3a8e60ecb9214f8db1d771e
Gahan <gahan@quixom.com>
2017-11-21 15:01:19 +0530

    redesign url

--- employee_management/urls.py
+++ employee_management/urls.py
@@ -5,6 +5,7 @@ from django.contrib import admin
 
 urlpatterns = [
     url(r'', include('main.urls')),
+    url(r'', include('employee.urls')),
     url(r'^admin/', admin.site.urls),
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
 ]
