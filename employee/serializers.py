43847b9cf3339a3ad6062ccbdce5bd6af13e3689
Gahan <gahan@quixom.com>
2017-11-24 12:28:36 +0530

    added serializers

---
+++ employee/serializers.py
@@ -0,0 +1,21 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from employee.models import Employee
+from main.serializers import UserSerializer, CompanySerializer
+
+User = get_user_model()
+
+
+class EmployeeSerializer(serializers.ModelSerializer):
+    user = UserSerializer()
+    added_by = UserSerializer()
+    company_name = CompanySerializer()
+
+    class Meta:
+        model = Employee
+        fields = ['url', 'id', 'user', 'company_name',
+                  'alternate_contact_no', 'alternate_email',
+                  'job_title', 'street', 'zip_code', 'city', 'country',
+                  'added_by', 'registration_date'
+                  ]
---
+++ main/serializers.py
@@ -0,0 +1,20 @@
+from django.contrib.auth import get_user_model
+from rest_framework import serializers
+
+from main.models import Company
+
+User = get_user_model()
+
+
+class UserSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = User
+        fields = ['url', 'contact_number', 'first_name', 'last_name', 'profile_image', 'email', 'password',
+                  'role', 'registration_date']
+        # extra_kwargs = {'password': {'write_only': True}}
+
+
+class CompanySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Company
+        fields = ["url", "id", "name", "alternate_contact_no", "alternate_email", "country", "company_user", ]
