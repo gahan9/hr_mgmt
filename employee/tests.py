f40c88217950204ae18df59dcbf9a83fe40d5377
Gahan9 <gahansaraiya@gmail.com>
2017-11-25 10:02:02 +0530

    test cases

--- employee/tests.py
+++ employee/tests.py
@@ -8,14 +8,14 @@ import random
 UserModel = get_user_model()
 
 
-def create_demo():
+def dummy_employee(role=3):
     number = random.randint(1000000000, 9999999999)
     name = "demo"
     email = "{0}@{0}.{0}".format(number)
     performer = UserModel.objects.get(id=7)
     company = Company.objects.get(id=2)
     user_obj = UserModel.objects.create(contact_number=number, email=email, first_name=name, last_name=name,
-                                        password=make_password(number), role=3, )
+                                        password=make_password(number), role=role, )
 
     ActivityMonitor.objects.create(activity_type=0, performed_by=performer, affected_user=user_obj)
     Employee.objects.create(user=user_obj, company_name=company, job_title='job_title', alternate_email=email,
