f216129e3363c337a49ae6809c50a5bd29e796ad
Gahan <gahan@quixom.com>
2017-12-11 18:52:47 +0530

    [progress update] step by step survey create

--- employee/serializers.py
+++ employee/serializers.py
@@ -1,5 +1,6 @@
 from django.contrib.auth import get_user_model
 from rest_framework import serializers
+from rest_framework.fields import MultipleChoiceField
 from rest_framework.reverse import reverse
 
 from employee.models import *
