2ca1cb7628b9e2a30a219d3ff2e76339cf899d5a
Gahan <gahan@quixom.com>
2018-01-05 19:37:54 +0530

    Added new UI files, integration started

--- forms/common.py
+++ forms/common.py
@@ -12,17 +12,16 @@ from employee.models import *
 
 class LoginForm(AuthenticationForm):
     """Form to allow user to log in to system"""
-    username = forms.CharField(label="Email", max_length=30,
+    username = forms.CharField(label="Contact Number", max_length=30,
                                widget=forms.TextInput(
-                                   attrs={'class': 'form-control', 'data-rule': 'email',
+                                   attrs={'class': 'form-control', 'type': 'text',
+                                          'aria-describedby': "emailHelp",
                                           'id': 'email', 'name': 'email',
-                                          'style': 'background:rgba(227, 231, 228, 1)',
-                                          'placeholder': 'Email'}))
+                                          'placeholder': 'Enter Contact Number'}))
     password = forms.CharField(label="Password", max_length=30,
                                widget=forms.TextInput(
                                    attrs={'class': 'form-control',
                                           'name': 'password', 'id': 'password',
-                                          'style': 'background:rgba(227, 231, 228, 1)',
                                           'type': 'password', 'placeholder': 'Password'}))
 
 
