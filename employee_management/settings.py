1fc2201fa2ec8ba59e9474039722da97da7b483f
Gahan <gahan@quixom.com>
2018-01-18 18:52:22 +0530

    [FEATURE] updated offset limit logic of surveylist api

--- employee_management/settings.py
+++ employee_management/settings.py
@@ -61,8 +61,8 @@ REST_FRAMEWORK = {
     'DEFAULT_PERMISSION_CLASSES': (
         'rest_framework.permissions.IsAuthenticated',
     ),
-    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.LimitOffsetPagination',
-    'PAGE_SIZE': 2,
+    'DEFAULT_PAGINATION_CLASS': 'rest_framework.pagination.PageNumberPagination',
+    'PAGE_SIZE': 5,
     'EXCEPTION_HANDLER': 'employee.utils.custom_exception_handler'
 }
 
