d2776c9ea791d3d268f0e34742108b11d60d414b
Gahan <gahan@quixom.com>
2018-02-13 13:05:13 +0530

    settings updated accordingly heroku

--- employee_management/server_settings.py
+++ employee_management/server_settings.py
@@ -3,13 +3,17 @@ MySQL database & settings for server
 """
 
 import os
+
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.mysql',
-        'OPTIONS': {
-            'read_default_file': os.path.join(BASE_DIR, 'mysql.conf'),
-        },
+        'ENGINE'  : 'django.db.backends.postgresql_psycopg2',
+        'NAME'    : 'd8e03fh4npq33h',
+        'USER'    : 'csfhhsgmfatlyh',
+        'PASSWORD': '8b05701836b0a11992277062dfe37a03d9e3437bdf029573f5972f2939fd76d8',
+        'HOST'    : 'ec2-184-73-199-189.compute-1.amazonaws.com',
+        'URI'     : 'postgres://csfhhsgmfatlyh:8b05701836b0a11992277062dfe37a03d9e3437bdf029573f5972f2939fd76d8@ec2-184-73-199-189.compute-1.amazonaws.com:5432/d8e03fh4npq33h',
+        'PORT'    : '5432',
     }
 }
