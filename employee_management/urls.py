883f5997fd4c4aec5e30071065d306561c37bc17
Gahan <gahan@quixom.com>
2018-03-26 12:24:33 +0530

    added doc urls

--- employee_management/urls.py
+++ employee_management/urls.py
@@ -2,8 +2,8 @@
 
 from django.conf.urls import url, include
 from django.contrib import admin
-# from rest_framework.authtoken import views as auth_token_views
 from main.views import CustomAuthentication
+from rest_framework.documentation import include_docs_urls
 
 urlpatterns = [
     url(r'', include('main.urls')),
@@ -12,4 +12,5 @@ urlpatterns = [
     url(r'^api-auth/', include('rest_framework.urls', namespace='rest_framework')),
     url(r'^api-token-auth/', CustomAuthentication.as_view(), name='get_auth_token'),
     url(r'^chat/', include('chat.urls')),
+    url(r'^api-docs/', include_docs_urls(title='Api doc')),
 ]
